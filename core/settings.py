d9286aa99c8443c6b8baf49ad6164bcf22639c62
rjkarthikeyan <rjkarthikeyan47@gmail.com>
2026-02-28 19:25:41 +0530

    Fixed static files and template inheritance

--- core/settings.py
+++ core/settings.py
@@ -17,10 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 T_dir= os.path.join(BASE_DIR, 'templates')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
